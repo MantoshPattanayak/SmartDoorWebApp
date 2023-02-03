7782e4adf32cd6dc18ba4be655e9e46484eb0f78
Betapoint22 <106087370+Betapoint22@users.noreply.github.com>
2023-02-03 18:06:28 +0530

    Update keonjhar_server_python_v2.0.py

--- keonjhar_server_python_v2.0.py
+++ keonjhar_server_python_v2.0.py
@@ -147,6 +147,7 @@ def device_status(filtered_data,topic_data):
 			cursor.execute(sql3,(current_time,device_id,school_id))
 			topic_alarm = topic_data+"Set_Alert"
 			client.publish(topic_alarm,"theft/alarm_on")
+			print(topic_data)
 			result = cursor.fetchall()
 			connection.commit()
 			print("Forced_Entry has been set off for device_id {} & school_id {}".format(device_id,school_id))
