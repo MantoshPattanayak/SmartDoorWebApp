5d1aca0494b2c9f7e92ae21e26fa017532ab44c2
Betapoint22 <106087370+Betapoint22@users.noreply.github.com>
2023-02-07 17:11:27 +0530

    Update device_heart_beat.py

--- device_heart_beat.py
+++ device_heart_beat.py
@@ -51,7 +51,7 @@ while True:
 				sql = "Select Timestampdiff(minute,%s,%s)"
 				cursor.execute(sql,(school_time,current_time))
 				result = cursor.fetchone()
-				print(result.values.())
+				print(result.values())
 				print(type(result))
 				# if result[''] > 1:
 				# 	print("Device {} offline".format(result[0]['device_id']))
