4345d05867391f928c30d1dd9e702480ef15ea69
Betapoint22 <106087370+Betapoint22@users.noreply.github.com>
2023-02-07 16:48:41 +0530

    Update device_heart_beat.py

--- device_heart_beat.py
+++ device_heart_beat.py
@@ -24,9 +24,22 @@ logger.info("Connected to MQTT")
 connection = pymysql.connect(host='souliot.mariadb.database.azure.com',user='okcliot@souliot',password='Siva@123',database='okcldb',cursorclass=pymysql.cursors.DictCursor)
 
 while True:
+	UTC = pytz.utc
+	IST = pytz.timezone('Asia/Kolkata')
+	today = date.today()
+	current_time = datetime.now(IST)
+	current_time =current_time.strftime('%H:%M:%S')
+	current_date = today.strftime("%d/%m/%Y")
+
 	client = mqtt.Client()
 	client.on_connect = on_connect
 	client.on_message = on_message
 	client.connect("20.205.208.135", 1883, 60)
 	time.sleep(4)
 	with connection.cursor() as cursor:
+		sql = "Select device_id,device_heart_beat_time from keonjhar_school_device"
+		cursor.execute(sql)
+		result = cursor.fetchall()
+		connection.commit()
+		print(type(result))
+		print(result)
