a8a8153f2ff5af803450d2475bfd3bebcb48f6e9
Betapoint22 <106087370+Betapoint22@users.noreply.github.com>
2023-02-08 15:20:59 +0530

    Update device_heart_beat.py

--- device_heart_beat.py
+++ device_heart_beat.py
@@ -59,8 +59,8 @@ while True:
 				with connection.cursor() as cursor:
 					sql11 = "Insert into keonjhar_log (log_type,log_school_id,log_device_id,log_description,log_time,log_date,log_alert_status) values(%s,%s,%s,%s,%s,%s,%s);"
 					sql33 = "update keonjhar_school_device set device_heartbeat_status = %s where device_id=%s and school_id=%s"
-					cursor.execute(sql22,("Offline",int(device_id),int(school_id)))
-					cursor.execute(sql33,("device_offline",int(school_id),int(device_id),"device Offline",str(current_time),str(current_date),"0"))
+					cursor.execute(sql33,("Offline",int(device_id),int(school_id)))
+					cursor.execute(sql11,("device_offline",int(school_id),int(device_id),"device Offline",str(current_time),str(current_date),"0"))
 					result = cursor.fetchall()
 					connection.commit()
 			else:
