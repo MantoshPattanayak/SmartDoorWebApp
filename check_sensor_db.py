4867ee872ffa6832fb59129f32874b6fb812c102
Betapoint22 <106087370+Betapoint22@users.noreply.github.com>
2023-02-20 12:56:27 +0530

    Update check_sensor_db.py

--- check_sensor_db.py
+++ check_sensor_db.py
@@ -21,19 +21,19 @@ def on_message(client, userdata, msg):
 
 while True:
 	try:
-		#connection = pymysql.connect(host='souliot.mariadb.database.azure.com',user='okcliot@souliot',password='Siva@123',database='okcldb',cursorclass=pymysql.cursors.DictCursor)
+		print("trying")
 		client = mqtt.Client()
 		client.on_connect = on_connect
 		client.on_message = on_message
 		client.connect("20.205.208.135", 1883, 60)		
 	except Exception as e:
+		print("Issue Found")
 		raise e
 	else:
-		print("Restarting")
-	connection = pymysql.connect(host='souliot.mariadb.database.azure.com',user='okcliot@souliot',password='Siva@123',database='okcldb',cursorclass=pymysql.cursors.DictCursor)
-	with connection.cursor() as cursor:
-		sql = "select school_id,device_id from keonjhar_school_device where device_temp>=50 and device_mq2>=5000 and device_hum<=25 group by school_id having count(device_temp)>=2"
-		cursor.execute(sql)
-		result = cursor.fetchall()
-		print(result)
+		connection = pymysql.connect(host='souliot.mariadb.database.azure.com',user='okcliot@souliot',password='Siva@123',database='okcldb',cursorclass=pymysql.cursors.DictCursor)
+		with connection.cursor() as cursor:
+			sql = "select school_id,device_id from keonjhar_school_device where device_temp>=50 and device_mq2>=5000 and device_hum<=25 group by school_id having count(device_temp)>=2"
+			cursor.execute(sql)
+			result = cursor.fetchall()
+			print(result)
 # client.loop_forever()
