7f58a380700afec8c5f39d7291d714c12bd3450f
Betapoint22 <106087370+Betapoint22@users.noreply.github.com>
2023-02-20 12:30:21 +0530

    Update check_sensor_db.py

--- check_sensor_db.py
+++ check_sensor_db.py
@@ -11,11 +11,10 @@ def on_connect(client, userdata, flags, rc):
 
 def on_message(client, userdata, msg):
     #print(msg.topic+" "+str(msg.payload))
-
-client = mqtt.Client()
-client.on_connect = on_connect
-client.on_message = on_message
-client.connect("20.205.208.135", 1883, 60)
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.on_message = on_message
+    client.connect("20.205.208.135", 1883, 60)
 
 
 global data
